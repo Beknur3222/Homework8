4b2020ea231e0b5abfdc23aff96198134979cc59
Бекнур Саттар <www.asemush@gmail.com>
2023-11-05 21:18:18 +0600

    Добавьте файлы проекта.

---
+++ Homework8/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    class SquaredArray
+    {
+        private double[] data; 
+
+        public SquaredArray(int size)
+        {
+            data = new double[size];
+        }
+
+ 
+        public double this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < data.Length)
+                {
+                    return data[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < data.Length)
+                {
+                    data[index] = value * value; 
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            SquaredArray arr = new SquaredArray(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Введите значение для arr[{i}]: ");
+                double value = double.Parse(Console.ReadLine());
+                arr[i] = value; 
+
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"arr[{i}] = {arr[i]}"); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tapsyrma2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tapsyrma2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Расчет коммунальных платежей");
+
+            Console.Write("Площадь помещения (м2): ");
+            double area = double.Parse(Console.ReadLine());
+
+            Console.Write("Количество проживающих людей: ");
+            int numberOfResidents = int.Parse(Console.ReadLine());
+
+            Console.Write("Сезон (осень/зима - 1, весна/лето - 2): ");
+            int season = int.Parse(Console.ReadLine());
+
+            Console.Write("Льготы (ветеран труда - 1, ветеран войны - 2, нет льгот - 3): ");
+            int discountType = int.Parse(Console.ReadLine());
+
+            double heatingRate = 10; 
+            double waterRate = 5; 
+            double gasRate = 8;
+            double repairRate = 7; 
+
+            double heatingPayment = area * heatingRate;
+            double waterPayment = numberOfResidents * waterRate;
+            double gasPayment = numberOfResidents * gasRate;
+            double repairPayment = area * repairRate;
+
+            if (season == 1)
+            {
+                heatingPayment *= 1.2;
+            }
+
+            double discount = 0;
+            if (discountType == 1)
+            {
+                discount = (heatingPayment + repairPayment) * 0.3;
+            }
+            else if (discountType == 2)
+            {
+                discount = (heatingPayment + repairPayment) * 0.5;
+            }
+
+            double totalPayment = heatingPayment + waterPayment + gasPayment + repairPayment - discount;
+
+            Console.WriteLine("Вид платежа\tНачислено\tЛьготная скидка\tИтого");
+            Console.WriteLine("Отопление\t{0:C2}\t{1:C2}\t{2:C2}", heatingPayment, discount, heatingPayment - discount);
+            Console.WriteLine("Вода\t{0:C2}\t0.00\t{0:C2}", waterPayment);
+            Console.WriteLine("Газ\t{0:C2}\t0.00\t{0:C2}", gasPayment);
+            Console.WriteLine("Ремонт\t{0:C2}\t{1:C2}\t{2:C2}", repairPayment, discount, repairPayment - discount);
+            Console.WriteLine("Итого\t{0:C2}\t{1:C2}\t{2:C2}", totalPayment + discount, discount, totalPayment);
+
+            Console.ReadKey();
+        }
+    }
+}
